a564e2499de2b326a809feafd60d018af4006ef7
Celia_Wu <530081999@qq.com>
2019-08-08 13:14:15 +0800

    Add files via upload
    这是两个用来新增ProblemList的文件，只使用爬虫功能的用户无需管理或使用。

---
+++ ProblemListGenerator.py
@@ -0,0 +1,11 @@
+# 对应的读取文件格式请参考同文件夹下的 1.txt 文件
+dic = {}
+with open("1.txt", "r") as f:
+    ff = f.readlines()
+    for line in ff:
+        
+        print (line.split("\t"))
+        num, title = line.split("\t")
+        title = title[:-1]
+        dic[title] = int(num)
+print (dic)
