fab7bb84fccc40a01bfab1ed23bf4da91f311a31
Jiayang Wu <530081999@qq.com>
2023-06-06 10:05:17 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 import os
 import time
+import json
 
 FILE_FORMAT = {"C++": ".cpp", "Python3": ".py", "Python": ".py", "MySQL": ".sql", "Go": ".go", "Java": ".java",
                "C": ".c", "JavaScript": ".js", "TypeScript": ".ts", "PHP": ".php", "C#": ".cs", "Ruby": ".rb", "Swift": ".swift",
                "Scala": ".scl", "Kotlin": ".kt", "Rust": ".rs"}
+
+TEMP_FILE_PATH = "./temp_problemset.txt"
 
 
 def generatePath(problem_id, problem_title, submission_language, OUTPUT_DIR):
@@ -34,3 +37,57 @@ def gitPush(OUTPUT_DIR):
     for instruction in instructions:
         os.system(instruction)
         print("~~~~~~~~~~~~~" + instruction + " finished! ~~~~~~~~")
+
+
+def wrap_up_scraping(not_found_list, problems_to_be_reprocessed, update_problemset, MAPPING):
+    if not_found_list:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Warning: Writes for following problems failed due to unknown Problem id!")
+        print("This issue can be solved by updating problemset")
+        for problem_title in not_found_list:
+            print(problem_title)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+    # 处理暂时题号
+    # 二次处理新题目，以永久题号替代暂时题号
+    with open(TEMP_FILE_PATH, "r") as f:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Now deal with problems with temporary problem IDs.")
+        print("Renaming or deleting would be needed.")
+        for line in f.readlines():
+            title, old_path = line.rstrip().split(" ", 1)
+            old_problem_id, problem_title = title.split("-", 1)
+
+            permanent_problem_id = MAPPING.get(problem_title, 0)
+            new_problem_id = '{:0>4}'.format(permanent_problem_id)
+            if not permanent_problem_id:
+                problems_to_be_reprocessed.append((title, old_path))
+            else:
+                new_path = old_path.replace(old_problem_id, new_problem_id)
+
+            old_dir_path, old_file_name = os.path.split(old_path)
+            new_dir_path, new_file_name = os.path.split(new_path)
+            # 确保新文件所在的文件夹存在
+            if os.path.exists(new_path):
+                # if new path exists, just delete old path
+                print(f"{new_file_name} exists, {old_file_name} will be deleted")
+                os.remove(old_path)
+                os.rmdir(old_dir_path)
+                continue
+            os.makedirs(new_dir_path, exist_ok=True)
+            os.rename(old_dir_path, new_dir_path)
+            os.rename(os.path.join(new_dir_path, old_file_name), new_path)
+            print(f"{old_file_name} has been renamed to {new_file_name}")
+
+    os.remove(TEMP_FILE_PATH)
+
+    # 把暂时题号的题目写到本地
+    if problems_to_be_reprocessed:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print("The following problems have been recoreded to local logs for reprocessing, ")
+        print("when their permenant problem IDs become available.")
+        with open(TEMP_FILE_PATH, "w") as f:
+            for title, path in problems_to_be_reprocessed:
+                f.write(title + " " + path + "\n")
+                print(title)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
